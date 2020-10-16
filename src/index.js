bd83a1644992cae8dede23b4261e9a8e25c1aefe
Burhonov <38163059+Burhonov@users.noreply.github.com>
2020-10-16 18:17:45 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+  let number = n < 0 ? n * (-1) : n;
+  while (number >= 1) {
+      result = result * 10 + number % 10;
+      number = Math.floor(number / 10);
+  }
+  return result;
 }
